fcca64d93cd7da52adbf556183bc39883c5121d9
adriannahdbell@gmail.com <120890577+thecodedcosmos@users.noreply.github.com>
2023-09-10 14:21:16 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
-}
+    const button = document.getElementById('button');
+  
+    function buttonClick() {
+      alert('Button clicked!');
+    }
+  
+    button.addEventListener('click', buttonClick);
+  }
+  
+  addingEventListener();
